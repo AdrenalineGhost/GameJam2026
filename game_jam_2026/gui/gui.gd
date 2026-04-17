2631a9543924ee07a35452b4f101036a9571576a
Zepje <seppe.landtsheer@gmail.com>
2026-04-17 13:16:55 +0200

    gui werkt apart

---
+++ game_jam_2026/gui/gui.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+var currentHealth
+var currentWave
+
+func _ready():
+	GuiManager.change_health_signal.connect(on_health_changed)
+	GuiManager.change_wave_signal.connect(on_wave_changed)
+
+func on_health_changed(new_health):
+	currentHealth = new_health
+	$health.value = currentHealth
+	
+func on_wave_changed(new_wave):
+	currentWave = new_wave
+	$wave_counter.text = "Wave %s" % new_wave
